d607a4b546d4762efae1525a0c6e29bfaa07b44f
Ammar <bc160400777@vu.edu.pk>
2025-08-31 00:27:30 +0500

    changes  upload files

--- WebProject/UploadFile.aspx.cs
+++ WebProject/UploadFile.aspx.cs
@@ -28,18 +28,22 @@ namespace WebProject
         public static string UploadFiles()
         {
             string firstError = "";
-            string uploadPath = "~/Uploads/";
+            string uploadPath = @"E:\Uploads"; //physical path on server to save files
             try
             {
-
                 string UserID = HttpContext.Current.Session["UserId"].ToString();
                 var httpRequest = HttpContext.Current.Request;
                 var files = httpRequest.Files;
-                var savePath = HttpContext.Current.Server.MapPath(uploadPath);
+                var savePath = uploadPath;
+
+                // Ensure directory exists
+                if (!Directory.Exists(savePath))
+                {
+                    Directory.CreateDirectory(savePath);
+                }
 
                 if (files.Count > 0)
                 {
-
                     string FileGuid = httpRequest.Form["FileGuid"];
                     string DocType = httpRequest.Form["DocType"];
                     string newFileName = "";
@@ -50,108 +54,79 @@ namespace WebProject
                         string OriginalFileName = Path.GetFileName(postedFile.FileName);
                         long fileSizeBytes = postedFile.ContentLength;
                         string fSize = "";
+
                         if (fileSizeBytes < 1024)
-                        {
                             fSize = fileSizeBytes + " Bytes";
-                        }
-                        else if ((fileSizeBytes/1024) < 1024 )
-                        {
+                        else if ((fileSizeBytes / 1024) < 1024)
                             fSize = (fileSizeBytes / 1024) + " KBs";
-
-                        }
-                        else //if ((fileSizeBytes / (1024*1024)) < 1024)
-                        {
+                        else
                             fSize = (fileSizeBytes / (1024 * 1024)) + " MBs";
-
-                        }
-                        //fileSizeBytes = postedFile.ContentLength /1024;
 
                         var FURecId = SaveFileToDb(postedFile.FileName, fSize, UserID);
                         var arrFURecId = FURecId.Split(new string[] { "||" }, StringSplitOptions.RemoveEmptyEntries);
-                        // Combine the prefixes and the original file name
+
                         if (arrFURecId.Length > 1)
                         {
                             newFileName = $"{arrFURecId[0]}_{FileGuid}_{OriginalFileName}";
-
                             var filePath = Path.Combine(savePath, newFileName);
                             postedFile.SaveAs(filePath);
                         }
                     }
-
-
-
-
                 }
             }
             catch (Exception e)
             {
                 firstError = e.Message;
             }
+
             try
             {
                 return GetUploadedFileList();
             }
             catch (Exception ex)
             {
-                return $@"{{""Status"":""Error"",""Response"":{{message:""{firstError} - {ex.Message}""}}}}";
+                return $@"{{""Status"":""Error"",""Response"":{{""message"":""{firstError} - {ex.Message}""}}}}";
             }
         }
-        private static string SaveFileToDb(string fileName,string FileSize, string UserID)
+
+        private static string SaveFileToDb(string fileName, string FileSize, string UserID)
         {
             var PD = GetParamsFromPost();
             PD.Add("FileName", new QueryParameter(fileName));
-            PD.Add("FileSize", new QueryParameter(FileSize.ToString())); // Add DBAction parameter
-            //PD.Add("DocType", new QueryParameter(DocType));
+            PD.Add("FileSize", new QueryParameter(FileSize));
             PD.Add("DBAction", new QueryParameter("AddUploadedFile"));
             PD.Add("UserId", new QueryParameter(UserID));
-            return  DBHelper.InvokeSP("FX_UPD_FileUpload_SP", PD).ToString();
-
-            // DBHelper.GetDataTableProc("SPName", PD);
+            return DBHelper.InvokeSP("FX_UPD_FileUpload_SP", PD).ToString();
         }
-
-
 
         static ParamDictionary<string, AsyncWidgets.DAL.QueryParameter> GetParamsFromPost()
         {
             var request = HttpContext.Current.Request;
-            var PD =
-                new ParamDictionary<string, AsyncWidgets.DAL.QueryParameter>("UploadFile2", "");
+            var PD = new ParamDictionary<string, AsyncWidgets.DAL.QueryParameter>("UploadFile2", "");
             foreach (var prm in request.Form.AllKeys)
             {
                 PD.Add(prm, new QueryParameter(request.Form[prm]));
-
             }
-
             return PD;
         }
 
         static string GetUploadedFileList()
         {
-            var a = 0;
             var PD = GetParamsFromPost();
-            //
-            PD.Add("DBAction", new QueryParameter("GetUploadedFiles")); // Add DBAction parameter
+            PD.Add("DBAction", new QueryParameter("GetUploadedFiles"));
             DataSet ds = DBHelper.GetDataTableProc("FX_UPD_FileUpload_SP", PD);
-
-
 
             if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
             {
-                // var rows = JsonConvert.SerializeObject(ds.Tables[0], new Newtonsoft.Json.Converters.JavaScriptDateTimeConverter());
                 var obj = new
                 {
                     status = "OK",
                     Response = new { Rows = ds.Tables[0], Count = ds.Tables[0].Rows.Count }
                 };
-
-
                 return JsonConvert.SerializeObject(obj, new Newtonsoft.Json.Converters.JavaScriptDateTimeConverter());
-
-                //$@"{{status:""OK"",response:{{Rows:{rows},Count:{ds.Tables[0].Rows.Count}}}}}";
             }
-            //var a=@"""Ammar""";
-            return @"{status:""No Files Found"",response:""""}";
 
+            return @"{""status"":""No Files Found"",""response"":""""}";
         }
     }
 }
